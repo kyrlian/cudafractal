ef401cb09194295129daf00bd2cdaaeca20ea5eb
kyrlian <63187993+kyrlian@users.noreply.github.com>
2022-12-20 21:09:07 +0100

    debug

--- cudafractal.py
+++ cudafractal.py
@@ -12,7 +12,7 @@ from numba import float64, int8, complex128
 from math import log
 
 
-@cuda.jit('void(int8[:,:,:], int32, int32, int32, int32)', device=True)
+@cuda.jit("void(int8[:,:,:], int32, int32, int32, int32)", device=True)
 def set_image_color(image_array, x, y, iterations, max_iterations):
     if iterations == max_iterations:  # BLACK
         return
@@ -36,12 +36,12 @@ def set_image_color(image_array, x, y, iterations, max_iterations):
     image_array[y, x, 2] = int8(b * 255)
 
 
-@cuda.jit('void(int8[:,:,:], complex128, float64, float64, int32)')
+@cuda.jit("void(int8[:,:,:], complex128, float64, float64, int32)")
 def mandelbrot(image_array, topleft, xstride, ystride, max_iter):
     y, x = cuda.grid(2)
     if x < image_array.shape[1] and y < image_array.shape[0]:
         c = complex128(topleft + x * xstride - 1j * y * ystride)
-        z = complex128(0+0j)
+        z = complex128(0 + 0j)
         nbi = 0
         while nbi < max_iter and z.real * z.real + z.imag * z.imag < 4:
             z = z * z + c
@@ -49,19 +49,9 @@ def mandelbrot(image_array, topleft, xstride, ystride, max_iter):
         set_image_color(image_array, x, y, nbi, max_iter)
 
 
-def run_kernel(kernel, image_array, topleft, xstride, ystride, max_iter):
-    start = timeit.default_timer()
-    dimage = cuda.to_device(image_array)
-    threadsperblock = (32, 16)
-    blockspergrid = (math.ceil(image_array.shape[0] / threadsperblock[0]), math.ceil(image_array.shape[1] / threadsperblock[1]))
-    #blockspergrid = (image_array.shape[0] + threadsperblock[0] - 1) // threadsperblock[0], (image_array.shape[1] + threadsperblock[1] - 1) // threadsperblock[1]
-    kernel[blockspergrid, threadsperblock](dimage, topleft, xstride, ystride, max_iter)
-    dimage.copy_to_host()
-    sys.stdout.write('\r' + "Fractal calculated on GPU in %f s" %
-                     (timeit.default_timer() - start))
+def create_image(xmin, xmax, ymin, ymax, max_iter, base_accuracy):
+    timerstart = timeit.default_timer()
 
-
-def create_image_array(kernel, xmin, xmax, ymin, ymax, max_iter, base_accuracy):
     if abs(xmax - xmin) > abs(ymax - ymin):
         ny = base_accuracy
         nx = int((base_accuracy * abs(xmax - xmin) / abs(ymax - ymin)))
@@ -72,14 +62,27 @@ def create_image_array(kernel, xmin, xmax, ymin, ymax, max_iter, base_accuracy):
     ystride = abs(ymax - ymin) / ny
     topleft = complex128(xmin + 1j * ymax)
     image_array = numpy.zeros((ny, nx, 3), dtype=numpy.uint8)
-    run_kernel(kernel, image_array, topleft, xstride, ystride, max_iter)
-    return image_array
 
+    # device_array = cuda.to_device(array_of_random)
+    # device_output_array = cuda.device_array(size)
+    # kernel[512, 512](device_array,  .5, device_output_array)
+    # output_array = device_output_array.copy_to_host()
 
-def display_image(xmin, xmax, ymin, ymax, max_iterations, base_accuracy):
-    # Call the kernel to render the fractal
-    image_array = create_image_array(
-        mandelbrot, xmin, xmax, ymin, ymax, max_iterations, base_accuracy)
+    device_image = cuda.to_device(image_array)
+    threadsperblock = (32, 16)
+    blockspergrid = (
+        math.ceil(image_array.shape[0] / threadsperblock[0]),
+        math.ceil(image_array.shape[1] / threadsperblock[1]),
+    )
+    # blockspergrid = (image_array.shape[0] + threadsperblock[0] - 1) // threadsperblock[0], (image_array.shape[1] + threadsperblock[1] - 1) // threadsperblock[1]
+    mandelbrot[blockspergrid, threadsperblock](
+        device_image, topleft, xstride, ystride, max_iter
+    )
+    image_array = device_image.copy_to_host()
+
+    sys.stdout.write(
+        "\r" + "Frame calculated in %f s" % (timeit.default_timer() - timerstart)
+    )
     pyplot.imshow(image_array)
     pyplot.show()
     return image_array
@@ -90,26 +93,25 @@ xmin, xmax, ymin, ymax = -2.0, 1.0, -1.5, 1.5
 base_accuracy = 1024
 max_iterations = 255
 
-image_array = display_image(xmin, xmax, ymin, ymax,
-                            max_iterations, base_accuracy)
+image_array = create_image(xmin, xmax, ymin, ymax, max_iterations, base_accuracy)
 
 # Handle user click events
 
 
 def click(event):
     mouseX, mouseY = int(event.xdata), int(event.ydata)
-    if event.button == 'left':
+    if event.button == "left":
         # Zoom in
-        xmin = xmin+mouseX*(xmax-xmin)/image_array.shape[0]-(xmax-xmin)/4
-        ymin = ymin+mouseY*(ymax-ymin)/image_array.shape[1]-(ymax-ymin)/4
-        xmax = xmin+(xmax-xmin)/2
-        ymax = ymin+(ymax-ymin)/2
-    elif event.button == 'right':
+        xmin = xmin + mouseX * (xmax - xmin) / image_array.shape[0] - (xmax - xmin) / 4
+        ymin = ymin + mouseY * (ymax - ymin) / image_array.shape[1] - (ymax - ymin) / 4
+        xmax = xmin + (xmax - xmin) / 2
+        ymax = ymin + (ymax - ymin) / 2
+    elif event.button == "right":
         # Zoom out
-        xmin = xmin-(xmax-xmin)/2
-        ymin = ymin-(ymax-ymin)/2
-        xmax = xmin+(xmax-xmin)*2
-        ymax = ymin+(ymax-ymin)*2
+        xmin = xmin - (xmax - xmin) / 2
+        ymin = ymin - (ymax - ymin) / 2
+        xmax = xmin + (xmax - xmin) * 2
+        ymax = ymin + (ymax - ymin) * 2
     else:
         return
-    display_image(xmin, xmax, ymin, ymax, max_iterations, base_accuracy)
+    create_image(xmin, xmax, ymin, ymax, max_iterations, base_accuracy)
