4495ed082db34e5ff0cccc6d0b458ad2c51a2975
kyrlian <kyrlian@gmail.com>
2024-12-07 14:52:10 +0100

    test

--- test/test-np-vectorize.py
+++ test/test-np-vectorize.py
@@ -19,9 +19,9 @@ def array_func(x, y, v):
     return x+y, v+1
 
 vectorized_array_func = np.vectorize(array_func, otypes=[np.int32,np.int32])
+
 x_array = [1, 2, 3, 4]
 y_array = [10, 20, 30, 40]
-v_array = [2]
-
+v_array = 2
 result_array2 = vectorized_array_func(x_array,y_array,v_array)
 print(f"vectorized_array_func result_array:{result_array2}")
